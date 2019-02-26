03f7dc50571af1ca7c5ac15cf494fd097d59f50c
Zachary Ferreira <Pearlybrah@github.com>
2019-02-26 14:51:14 -0800

    changed start on server.js

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var db = require("./models");
 var PORT = 3000;
 var app = express();
-const routes = require("./routes");
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+const routes = require("./routes");
 app.use(routes);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+mongoose.connect(MONGODB_URI);
