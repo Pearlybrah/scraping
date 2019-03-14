e52eee1dcd55aa83d4da0c56d750cafd3b8fc981
Zachary Ferreira <Pearlybrah@github.com>
2019-03-13 20:42:15 -0700

    heroku4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var router = require("express").Router();
 var apiRoutes = require("./api");
+var htmlRoutes = require("./html");
 
 router.use("/", apiRoutes);
+router.use("/", htmlRoutes);
 
 module.exports = router;
